5bc6fcd4d3a981b6dce134328f4ebc65e3f4aea1
Tom Stratton <tom.stratton@pure-energi.com>
2025-02-11 15:43:42 +0000

    set non sensitive env vars in start_server.sh

--- app/scripts/start_server.sh
+++ app/scripts/start_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+export AWS_REGION="eu-west-2"
+export SES_FROM_EMAIL="noreply@bakehouse78.co.uk"
+export SES_TO_EMAIL="tstratton3912@gmail.com"
+export NODE_ENV="production"
+
 # Stop any existing server
 pm2 stop bakehouse78 || true
 
