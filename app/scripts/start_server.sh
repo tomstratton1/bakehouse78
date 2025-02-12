bf7bf8e57aa4dff0cc194370a8703d9483f178c7
Tom Stratton <tom.stratton@pure-energi.com>
2025-02-12 11:50:03 +0000

    set ses to email to georgina

--- app/scripts/start_server.sh
+++ app/scripts/start_server.sh
@@ -2,7 +2,7 @@
 
 export AWS_REGION="eu-west-2"
 export SES_FROM_EMAIL="noreply@bakehouse78.co.uk"
-export SES_TO_EMAIL="tstratton3912@gmail.com"
+export SES_TO_EMAIL="georgina@bakehouse78.co.uk"
 export NODE_ENV="production"
 
 # Stop any existing server
