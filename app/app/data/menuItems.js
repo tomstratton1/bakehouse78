9350be50ea4b2f3e303b775974ec8cdb31eca84b
Tom Stratton <tom.stratton@pure-energi.com>
2025-03-04 13:57:57 +0000

    more photos

--- app/app/data/menuItems.js
+++ app/app/data/menuItems.js
@@ -6,7 +6,49 @@ const menuItems = [
         title: 'Focaccia',
         description: 'Indulge in our signature focaccia, freshly baked slabs handmade with care. Enjoy it on its own, as a sandwich base, or paired with your favourite dishes.',
         ingredients: 'Ingredients Placeholder',
-        image: 'bread1.jpg',
+        image: 'images/bread1.jpg',
+      },
+      {
+        title: 'Quiche of the Day',
+        description: 'Our quiches are crafted daily with premium ingredients, offering a perfect blend of flavours and textures.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/oven1.jpg',
+      },
+      {
+        title: 'Goat Cheese Tart',
+        description: 'A delicious tart featuring creamy goat cheese and fresh herbs.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/goat_cheese_tart1.jpg',
+      },
+      {
+        title: 'Sweet Potato and Black Bean Roll',
+        description: 'A vegetarian delight packed with sweet potatoes and black beans wrapped in flaky pastry.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/sweetpot_blackbean1.jpg',
+      },
+      {
+        title: 'BBQ Pulled Pork Rolls',
+        description: 'Tender pulled pork in a rich BBQ sauce, encased in a soft, freshly baked roll.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/sausage_roles_plate2.jpg',
+      },
+      {
+        title: 'Pork and Herb Sausage Rolls',
+        description: 'Classic sausage rolls made with high-quality pork and aromatic herbs.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/sausage_roles_plate.jpg',
+      },
+      {
+        title: 'Pork and Stilton Rolls',
+        description: 'A bold twist on traditional sausage rolls, featuring pork and Stilton cheese.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'comingsoon.jpg',
+      },
+      {
+        title: 'Pork, Bacon, Stuffing Rolls',
+        description: 'A hearty roll filled with pork, bacon, and flavorful stuffing.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'comingsoon.jpg',
       },
       {
         title: 'Salad of the Day',
@@ -21,69 +63,33 @@ const menuItems = [
         image: 'comingsoon.jpg',
       },
       {
-        title: 'Quiche of the Day',
-        description: 'Our quiches are crafted daily with premium ingredients, offering a perfect blend of flavours and textures.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Goat Cheese Tart',
-        description: 'A delicious tart featuring creamy goat cheese and fresh herbs.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
         title: 'Feta and Chilli Honey Knots',
         description: 'Soft, flavourful knots infused with feta cheese and a hint of chilli honey glaze.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Sweet Potato and Black Bean Roll',
-        description: 'A vegetarian delight packed with sweet potatoes and black beans wrapped in flaky pastry.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'BBQ Pulled Pork Rolls',
-        description: 'Tender pulled pork in a rich BBQ sauce, encased in a soft, freshly baked roll.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Pork and Herb Sausage Rolls',
-        description: 'Classic sausage rolls made with high-quality pork and aromatic herbs.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'sausageroles1.jpg',
-      },
-      {
-        title: 'Pork and Stilton Rolls',
-        description: 'A bold twist on traditional sausage rolls, featuring pork and Stilton cheese.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Pork, Bacon, Stuffing Rolls',
-        description: 'A hearty roll filled with pork, bacon, and flavorful stuffing.',
         ingredients: 'Ingredients Placeholder',
         image: 'comingsoon.jpg',
       },
     ],
   },
   {
-    category: 'Others',
+    category: 'Assorted',
     items: [
       {
         title: 'Almond Croissant',
         description: 'A flaky, buttery croissant filled with sweet almond paste.',
         ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
+        image: 'images-web/almond_croissant1.jpg',
       },
       {
         title: 'Chocolate Croissant',
         description: 'A classic pastry with layers of buttery dough and rich chocolate filling.',
         ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
+        image: 'images-web/pain_choc1.jpg',
+      },
+      {
+        title: 'Cheese Scones',
+        description: 'Savory cheese scones, perfect with butter or a warm bowl of soup.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/scones_tray1.jpg',
       },
       {
         title: 'Granola Pots',
@@ -95,7 +101,7 @@ const menuItems = [
         title: 'Cinnamon Focaccia',
         description: 'A sweet take on our signature focaccia, infused with cinnamon and sugar.',
         ingredients: 'Ingredients Placeholder',
-        image: 'bread1.jpg',
+        image: 'comingsoon.jpg',
       },
       {
         title: 'Cinnamon Buns',
@@ -112,12 +118,6 @@ const menuItems = [
       {
         title: 'Fruit Scone',
         description: 'A classic fruit scone packed with raisins and a hint of citrus zest.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Cheese Scones',
-        description: 'Savory cheese scones, perfect with butter or a warm bowl of soup.',
         ingredients: 'Ingredients Placeholder',
         image: 'comingsoon.jpg',
       },
@@ -139,6 +139,18 @@ const menuItems = [
     category: 'Special Enquiries',
     items: [
       {
+        title: 'Grazing Boards',
+        description: 'An assortment of sweet and savory delights for gatherings and celebrations.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/grazing1.jpg',
+      },
+      {
+        title: 'Corporate Lunches',
+        description: 'Tailored catering solutions for business meetings and corporate events.',
+        ingredients: 'Ingredients Placeholder',
+        image: 'images-web/selection1.jpg',
+      },
+      {
         title: 'Birthday Cakes',
         description: 'Custom-made birthday cakes tailored to your preferences and occasions.',
         ingredients: 'Ingredients Placeholder',
@@ -153,18 +165,6 @@ const menuItems = [
       {
         title: 'Wedding Cakes',
         description: 'Elegant and beautifully crafted wedding cakes for your big day.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Grazing Boards',
-        description: 'An assortment of sweet and savory delights for gatherings and celebrations.',
-        ingredients: 'Ingredients Placeholder',
-        image: 'comingsoon.jpg',
-      },
-      {
-        title: 'Corporate Lunches',
-        description: 'Tailored catering solutions for business meetings and corporate events.',
         ingredients: 'Ingredients Placeholder',
         image: 'comingsoon.jpg',
       },
--- app/app/menu/page.tsx
+++ app/app/menu/page.tsx
@@ -9,31 +9,6 @@ export default function MenuPage() {
       <Navbar />
       <section className="py-16 bg-offWhite text-center">
         <h1 className="text-6xl font-bold text-charcoal mb-8">Menu</h1>
-        {menuItems.map((category, index) => (
-          <div key={index} className="mb-12">
-            <h2 className="text-4xl font-semibold text-charcoal mb-8">{category.category}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12 px-12">
-              {category.items.map((item, idx) => (
-                <div key={idx} className="border rounded-lg overflow-hidden shadow-lg">
-                  <div className="h-64 bg-gray-300 flex items-center justify-center">
-                    <Image
-                      src={`/images/${item.image}`}
-                      alt={item.title}
-                      width={300}
-                      height={450}
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                  <div className="p-4">
-                    <h3 className="text-2xl font-bold mb-2 text-charcoal">{item.title}</h3>
-                    <p className="text-gray-700 mb-2">{item.description}</p>
-                    <p className="text-gray-500 text-sm mb-4">{item.ingredients}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
 
         {/* Sweet Bakes Section */}
         <div className="mb-12">
@@ -46,7 +21,7 @@ export default function MenuPage() {
                   <div key={idx} className="border rounded-lg overflow-hidden shadow-lg">
                     <div className="h-64 bg-gray-300 flex items-center justify-center">
                       <Image
-                        src={`/images/${item.image}`}
+                        src={`/${item.image}`}
                         alt={item.title}
                         width={300}
                         height={450}
@@ -56,7 +31,7 @@ export default function MenuPage() {
                     <div className="p-4">
                       <h3 className="text-2xl font-bold mb-2 text-charcoal">{item.title}</h3>
                       <p className="text-gray-700 mb-2">{item.description}</p>
-                      <p className="text-gray-500 text-sm mb-4">{item.ingredients}</p>
+                      {/* <p className="text-gray-500 text-sm mb-4">{item.ingredients}</p> */}
                     </div>
                   </div>
                 ))}
@@ -64,6 +39,34 @@ export default function MenuPage() {
             </div>
           ))}
         </div>
+
+        {/* Savoury and Other */}
+        {menuItems.map((category, index) => (
+          <div key={index} className="mb-12">
+            <h2 className="text-4xl font-semibold text-charcoal mb-8">{category.category}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12 px-12">
+              {category.items.map((item, idx) => (
+                <div key={idx} className="border rounded-lg overflow-hidden shadow-lg">
+                  <div className="h-64 bg-gray-300 flex items-center justify-center">
+                    <Image
+                      src={`/${item.image}`}
+                      alt={item.title}
+                      width={300}
+                      height={450}
+                      className="object-cover w-full h-full"
+                    />
+                  </div>
+                  <div className="p-4">
+                    <h3 className="text-2xl font-bold mb-2 text-charcoal">{item.title}</h3>
+                    <p className="text-gray-700 mb-2">{item.description}</p>
+                    {/* <p className="text-gray-500 text-sm mb-4">{item.ingredients}</p> */}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+
       </section>
     </main>
   );
