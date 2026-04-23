daf100e99cb27e9af8e1888d5dcb2ed9c85fe1b4
tomstratton1 <tstratton3912@gmail.com>
2026-04-23 13:51:17 +0100

    add api gw terraform

---
+++ iac/aws/api-gateway.tf
@@ -0,0 +1,54 @@
+resource "aws_apigatewayv2_api" "api" {
+  name          = "bakehouse78-api"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["https://tomstratton1.github.io"]  # or your domain later
+    allow_methods = ["GET"]
+    allow_headers = ["*"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "get_products" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "AWS_PROXY"
+
+  description     = "Integration for GET /products"
+  integration_uri    = aws_lambda_function.get_products.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "get_products" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /products"
+  
+  target = "integrations/${aws_apigatewayv2_integration.get_products.id}"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "dev"
+  auto_deploy = true
+  default_route_settings {
+    throttling_burst_limit = 50
+    throttling_rate_limit  = 20
+  }
+  access_log_settings {
+  destination_arn = aws_cloudwatch_log_group.api_logs.arn
+  format = jsonencode({
+    requestId = "$context.requestId"
+    ip        = "$context.identity.sourceIp"
+    routeKey  = "$context.routeKey"
+    status    = "$context.status"
+  })
+}
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_products.function_name
+  principal     = "apigateway.amazonaws.com"
+  
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
--- iac/aws/outputs.tf
+++ iac/aws/outputs.tf
@@ -1,7 +1,13 @@
+# DYNAMODB TABLE OUTPUTS
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.products.name
 }
 
 output "dynamodb_table_arn" {
   value = aws_dynamodb_table.products.arn
+}
+
+# API GATEWAY OUTPUTS
+output "api_url" {
+  value = aws_apigatewayv2_api.api.api_endpoint
 }
